0698cf608104b8f4c5664c478a34c58c078c1513
NAVEED476 <snaveedn23@gmail.com>
2025-05-18 21:29:10 +0530

    hoisiting problems

--- hoisting.js
+++ hoisting.js
@@ -1,3 +1,40 @@
-"use strict"
-x=23
-console.log(x)
+// "use strict"
+// x=23
+// console.log(x)
+
+
+
+greet();
+function greet() {
+  console.log("Hello!");
+}
+
+//ans Hello
+
+
+var speak = function (){
+    console.log("speak")
+}
+speak();
+
+// speak is not a function is the ans here why because the speak is decalred but we have initialised  a function it can not be hoisted
+
+
+var x = 10;
+function test() {
+  console.log(x);
+  var x = 20;
+}
+test();
+
+// the a
+
+var a = 1;
+function foo() {
+  console.log(a);
+  a = 2;
+}
+foo();
+console.log(a ,"this one gets execute first");
+
+
